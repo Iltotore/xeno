7758c5f7b5d7932bd45b7799c99ef14766d27246
Sebastian Klang <sebklang@gmail.com>
2025-03-27 12:09:43 +0100

    Amend: more compact formatting

--- src/Xeno/DOM/Decoding.hs
+++ src/Xeno/DOM/Decoding.hs
@@ -69,12 +69,12 @@ data DecodingCursor =
 
 showAsParent :: DecodingCursor -> String
 showAsParent RootCursor = ""
-showAsParent p          = show p ++ " -> "
+showAsParent p          = show p ++ " "
 
 instance Show DecodingCursor where
   show RootCursor = "root"
-  show (FieldCursor str p) = showAsParent p ++ "field " ++ str 
-  show (NodeCursor  str p) = showAsParent p ++ "node "  ++ str
+  show (FieldCursor str p) = showAsParent p ++ "field:" ++ str 
+  show (NodeCursor  str p) = showAsParent p ++ "node:"  ++ str
 
 -- |Get the parent of a given cursor.
 cursorParent :: DecodingCursor -> Maybe DecodingCursor
@@ -86,7 +86,7 @@ cursorParent (NodeCursor _ parent)  = Just parent
 data DecodingFailure = DecodingFailure DecodingCursor String
 
 instance Show DecodingFailure where
-  show (DecodingFailure cursor str) = "Decoding failure at " ++ show cursor ++ ", with message: " ++ str
+  show (DecodingFailure cursor str) = "Decoding failure at " ++ show cursor ++ " (" ++ str ++ ")"
 
 -- |An XML node decoder. Since a node can be decoded in several "natural" ways, this is not a typeclass.
 -- Instead, the goal is to provide some sort of DSL to easily describe the decoding logic.
